034c10c27ebd18355484fd742d81fc4f3bf34633
Daffa Rozano <daffarozano@gmail.com>
2021-04-18 17:33:13 +0700

    Update server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.set("view engine", "ejs");
 app.set("views", __dirname + "/views");
 app.set("layout", "layouts/layout");
 app.use(expressLayouts);
-app.use(express.static("/public"));
+app.use(express.static("public"));
 
 const mongoose = require("mongoose");
 mongoose.connect(process.env.DATABASE_URL, {
