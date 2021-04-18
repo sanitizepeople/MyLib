45c285fce0e2eddf8a46d6abc0032dbe5f8488c6
Daffa Rozano <daffarozano@gmail.com>
2021-04-19 00:44:04 +0700

    Update server.js

--- server.js
+++ server.js
@@ -14,7 +14,6 @@ app.set("views", __dirname + "/views");
 app.set("layout", "layouts/layout");
 app.use(expressLayouts);
 app.use(express.static("public"));
-<<<<<<< HEAD
 app.use(bodyParser.urlencoded({ limit: "10mb", extended: false }));
 =======
 
